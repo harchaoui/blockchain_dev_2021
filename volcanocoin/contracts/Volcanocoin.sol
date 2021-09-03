79aabd0b813815d646f3257db5fe685003dacfef
harchaoui <harchaoui.abdelali@gmail.com>
2021-09-03 22:53:24 +0000

    creating the volcanocoin smart contract and refactor the truffle migration

---
+++ volcanocoin/contracts/Volcanocoin.sol
@@ -0,0 +1,21 @@
+// contracts/Box.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Volcanocoin {
+  uint256  private _totalSupply  = 1000;
+  address owner;
+
+  modifier onlyOwner (){
+      if (msg.sender == owner){
+          _;
+      }
+  } 
+
+  function getTotalSupply() view public returns (uint256) {
+      return _totalSupply;
+  }
+  function increaseSupply() public onlyOwner {
+      _totalSupply +=1000;
+  }
+}
--- volcanocoin/migrations/1_initial_migration.js
+++ volcanocoin/migrations/1_initial_migration.js
@@ -1,5 +1,10 @@
 const Migrations = artifacts.require("Migrations");
+const Box = artifacts.require('Box');
+const Volcanocoin = artifacts,require('Volcanocoin');
+
 
 module.exports = function (deployer) {
   deployer.deploy(Migrations);
+  deployer.deploy(Box);
+  deployer.deploy(Volcanocoin);
 };
