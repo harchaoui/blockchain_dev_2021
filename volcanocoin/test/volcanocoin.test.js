e64132d48bdd11a1a9bbf499910025e462c008fb
harchaoui <harchaoui.abdelali@gmail.com>
2021-09-07 07:27:53 +0000

    testing volcanocoin total supply to be equal to 1000

--- volcanocoin/test/volcanocoin.test.js
+++ volcanocoin/test/volcanocoin.test.js
@@ -1,10 +1,20 @@
 const Vlc = artifacts.require('Volcanocoin');
 
 contract('vlc', () => {
-    
+
     it('Volcanocoin Should be deployed properly', async () => {
         const _vlc =  await Vlc.deployed();
-        console.log(_vlc.address); 
-        assert(_vlc.address !== '');
+        
+        assert(_vlc.address !== '' );
+
+        console.log(`volcanocoin lives @:${_vlc.address}`); 
+    
     });
+
+    it('Volcanocoin should has 1000 in supply', async ()=> {
+        const _vlc = await Vlc.deployed()
+        const totalSupply = await _vlc.getTotalSupply.call();
+        assert.equal(totalSupply, 1000 , "Minting failled")
+    }
+    );  
 });
