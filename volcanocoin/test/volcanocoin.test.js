520fd4a33d8bafc473c6f36620d14a12053b5c1c
harchaoui <harchaoui.abdelali@gmail.com>
2021-09-07 07:37:02 +0000

    refactor testing to add before() of mocha testing framework

--- volcanocoin/test/volcanocoin.test.js
+++ volcanocoin/test/volcanocoin.test.js
@@ -1,20 +1,18 @@
 const Vlc = artifacts.require('Volcanocoin');
 
 contract('vlc', () => {
-
-    it('Volcanocoin Should be deployed properly', async () => {
-        const _vlc =  await Vlc.deployed();
-        
-        assert(_vlc.address !== '' );
-
-        console.log(`volcanocoin lives @:${_vlc.address}`); 
-    
+    let _vlc = null;
+    before(async ()=>{
+    _vlc = await Vlc.deployed();
     });
 
-    it('Volcanocoin should has 1000 in supply', async ()=> {
-        const _vlc = await Vlc.deployed()
+        it('Volcanocoin Should be deployed properly', async () => {
+        assert(_vlc.address !== '' );
+        console.log(`volcanocoin lives @:${_vlc.address}`); 
+        });
+
+        it('Volcanocoin should has 1000 in supply', async ()=> {
         const totalSupply = await _vlc.getTotalSupply.call();
         assert.equal(totalSupply, 1000 , "Minting failled")
-    }
-    );  
+    });
 });
